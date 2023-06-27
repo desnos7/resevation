14b522abcaef2a5948de0d1fa1e0b011222cc63f
desnos7 <beugredesnosjeremie@gmail.com>
2023-06-27 17:24:30 +0000

    serve

--- api/model/transaction.js
+++ api/model/transaction.js
@@ -1,14 +1,13 @@
-import { mongoose } from 'mongoose';
+import mongoose from "mongoose";
 
 const { Schema } = mongoose;
 
 const transaction = new Schema({
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    product: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' },
-    amount: { type: Number, required: true },
-    paymentStatus: { type: String, required: true },
-  })
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  product: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+  amount: { type: Number, required: true },
+  paymentStatus: { type: String, required: true },
+});
 
-
-  const transactione=mongoose.model("reservez",transaction )
- export default transactione;
+const transactione = mongoose.model("reservez", transaction);
+export default transactione;
--- api/server.js
+++ api/server.js
@@ -3,9 +3,11 @@ import cors from "cors";
 import mongoose from "mongoose";
 import routeVehicule from "./routes/route.js";
 
-import {userVerification} from "./Middlewares/authMiddleware.js"
+import { userVerification } from "./Middlewares/authMiddleware.js";
 import sigupInscription from "./routes/incription.js";
 import cookieParser from "cookie-parser";
+import transactione from "./model/transaction.js";
+import Vehicule from "../src/pages/vehicules.jsx";
 
 const app = express();
 
@@ -23,7 +25,19 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use("/vehicules", routeVehicule);
-app.use("/post", sigupInscription); 
+app.use("/post", sigupInscription);
+app.use("/", async (req, res) => {
+  const tran = await transactione.create({
+    user: "jfkldjfkldjkfljds",
+    product: "kdjfkldsjfkdsjfdksl",
+    amount: 50000,
+    paymentStatus: "paid",
+  });
+
+  Vehicule =  await Vehicule.findByid(product)
+  Vehicule.paidTar = trans.id
+  a 
+});
 
 mongoose
   .connect(
